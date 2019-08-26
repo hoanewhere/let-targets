0c61981af4ea975e1d32b6144b4eb18a18441e26
i.naoto <e.h0andwhere@gmail.com>
2019-08-26 10:15:25 +0900

    fontsizeを修正

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="main-container">
-    <aside class="aside p-5 m-2">
+    <aside class="aside p-4 m-1">
         <h2 class="mb-4">絞り込み</h2>
         <form action="">
             <p class="mb-1">キーワード</p>
@@ -21,99 +21,99 @@
             <button type="submit" class="btn d-block">検索</button>
         </form>       
     </aside>
-    <article class="main-article m-2">
-        <div class="add-form p-5 mb-2">
+    <article class="main-article m-1">
+        <div class="add-form p-4 mb-2">
             <form action="">
                 <div class="form-row">
-                    <div class="form-group col-9">
+                    <div class="form-group col-8">
                         <label class="mb-1">新規目標</label>
-                        <input type="text" name="" id="" class="mb-4 form-control" placeholder="新規目標">
+                        <input type="text" name="" id="" class="form-control" placeholder="新規目標">
                     </div>
-                    <div class="form-group col-3">
+                    <div class="form-group col-4">
                         <label class="mb-1">完了予定日</label>
-                        <input type="date" name="" id="" class="mb-4 form-control">
+                        <input type="date" name="" id="" class="form-control">
                     </div>
                 </div>
                 <button type="submit" class="btn d-block">追加</button>
             </form>
         </div>
-        <div class="targets p-5">
+        <div class="targets p-4">
             <h2 class="mb-2">目標一覧</h2>
-            <div class="target m-3 p-3">
-                <div class="target-top p-3">
+            <div class="target m-2 p-2">
+                <div class="target-top p-2">
                     <p>ユーザー名</p>
                     <p>YYYY/MM/DD</p>
                 </div>
                 <div class="target-main">
-                    <i class="fas fa-check p-3"></i>
+                    <i class="fas fa-check p-2"></i>
                     <input type="text" name="" id="" class="form-control">
-                    <i class="far fa-trash-alt p-3"></i>
+                    <i class="far fa-trash-alt p-2"></i>
                 </div>
             </div>
-            <div class="target m-3 p-3">
-                <div class="target-top p-3">
+            <div class="target m-2 p-2">
+                <div class="target-top p-2">
                     <p>ユーザー名</p>
                     <p>YYYY/MM/DD</p>
                 </div>
                 <div class="target-main">
-                    <i class="fas fa-check p-3"></i>
+                    <i class="fas fa-check p-2"></i>
                     <input type="text" name="" id="" class="form-control">
-                    <i class="far fa-trash-alt p-3"></i>
+                    <i class="far fa-trash-alt p-2"></i>
                 </div>
             </div>
-            <div class="target m-3 p-3">
-                <div class="target-top p-3">
+            <div class="target m-2 p-2">
+                <div class="target-top p-2">
                     <p>ユーザー名</p>
                     <p>YYYY/MM/DD</p>
                 </div>
                 <div class="target-main">
-                    <i class="fas fa-check p-3"></i>
+                    <i class="fas fa-check p-2"></i>
                     <input type="text" name="" id="" class="form-control">
-                    <i class="far fa-trash-alt p-3"></i>
+                    <i class="far fa-trash-alt p-2"></i>
                 </div>
             </div>
-            <div class="target m-3 p-3">
-                <div class="target-top p-3">
+            <div class="target m-2 p-2">
+                <div class="target-top p-2">
                     <p>ユーザー名</p>
                     <p>YYYY/MM/DD</p>
                 </div>
                 <div class="target-main">
-                    <i class="fas fa-check p-3"></i>
+                    <i class="fas fa-check p-2"></i>
                     <input type="text" name="" id="" class="form-control">
-                    <i class="far fa-trash-alt p-3"></i>
+                    <i class="far fa-trash-alt p-2"></i>
                 </div>
             </div>
-            <div class="target m-3 p-3">
-                <div class="target-top p-3">
+            <div class="target m-2 p-2">
+                <div class="target-top p-2">
                     <p>ユーザー名</p>
                     <p>YYYY/MM/DD</p>
                 </div>
                 <div class="target-main">
-                    <i class="fas fa-check p-3"></i>
+                    <i class="fas fa-check p-2"></i>
                     <input type="text" name="" id="" class="form-control">
-                    <i class="far fa-trash-alt p-3"></i>
+                    <i class="far fa-trash-alt p-2"></i>
                 </div>
             </div>
-            <div class="target m-3 p-3">
-                <div class="target-top p-3">
+            <div class="target m-2 p-2">
+                <div class="target-top p-2">
                     <p>ユーザー名</p>
                     <p>YYYY/MM/DD</p>
                 </div>
                 <div class="target-main">
-                    <i class="fas fa-check p-3"></i>
+                    <i class="fas fa-check p-2"></i>
                     <input type="text" name="" id="" class="form-control">
-                    <i class="far fa-trash-alt p-3"></i>
+                    <i class="far fa-trash-alt p-2"></i>
                 </div>
             </div>
-            <div class="target m-3 p-3">
-                <div class="target-top p-3">
+            <div class="target m-2 p-2">
+                <div class="target-top p-2">
                     <p>ユーザー名</p>
                     <p>YYYY/MM/DD</p>
                 </div>
                 <div class="target-main">
-                    <i class="fas fa-check p-3"></i>
+                    <i class="fas fa-check p-2"></i>
                     <input type="text" name="" id="" class="form-control">
-                    <i class="far fa-trash-alt p-3"></i>
+                    <i class="far fa-trash-alt p-2"></i>
                 </div>
             </div>
         </div>
