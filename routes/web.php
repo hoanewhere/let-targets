ff41e229f3daf2f2e4cb96e4f56196a8bdb0dc1f
i.naoto <e.h0andwhere@gmail.com>
2019-08-28 10:11:35 +0900

    ルート先のurlを修正

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,10 @@ Auth::routes();
 // get
 Route::get('/', 'TargetController@index')->name('home');
 Route::get('/home', 'TargetController@index')->name('home');
-Route::get('/{id}/complete', 'TargetController@complete')->name('target.complete');
-Route::get('/{id}/notComplete', 'TargetController@notComplete')->name('target.notComplete');
-Route::get('/{id}/delete', 'TargetController@delete')->name('target.delete');
+Route::get('/index/{id}/complete', 'TargetController@complete')->name('target.complete');
+Route::get('/index/{id}/notComplete', 'TargetController@notComplete')->name('target.notComplete');
+Route::get('/index/{id}/delete', 'TargetController@delete')->name('target.delete');
+Route::get('/index/search', 'TargetController@search')->name('target.search');
 
 // post
 Route::post('/add', 'TargetController@add')->name('target.add');
